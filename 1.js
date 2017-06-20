1f753514e9c24ec53d604a0ce96c349cb6c2e8a0
ChungNguyen <lacduong9436@gmail.com>
2017-06-20 22:45:14 +0700

    demo

---
+++ 1.js
@@ -0,0 +1,17 @@
+﻿ $(function(){
+ $(window).scroll(function() {
+ 	if ($('body').scrollTop()>200){
+ 		$('.navbar').addClass('mauxanh');
+ 	}
+ 	else{
+ 		$('.navbar').removeClass('mauxanh');
+ 	}
+ 	
+ });
+ new WOW().init();
+ $('.so').counterUp({
+                delay: 1,
+                time: 3000,
+            });
+})  
+ 
